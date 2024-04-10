4b7787d1a1f975c9f20cf2b380cdaafd349ac1bd
garyu <49363894+garyu01@users.noreply.github.com>
2024-04-10 19:58:49 +0900

    Update MISS_effect.js

--- plagin/ミスエフェクト/MISS_effect.js
+++ plagin/ミスエフェクト/MISS_effect.js
@@ -1,46 +1,62 @@
 ﻿/*:
  * @target MZ
- * @plugindesc MISSエフェクト
- * @author 
- * @help 
- * MISSエフェクト
+ * @plugindesc 特定の条件でMISSアニメーションを表示するプラグイン
+ *
+ * @param TriggerWeapons
+ * @text トリガー武器ID
+ * @desc MISSアニメーションがトリガーされる武器ID
+ * @type number[]
+ * @default ["1","2","3","4","5"]
+ * 
+ * @param TriggerWeapons2
+ * @text トリガー武器ID2
+ * @desc 2番目の条件でMISSアニメーションがトリガーされる武器ID
+ * @type number[]
+ * @default ["6","7","8","9","10"]
  */
 
-
 (function() { 
-Sprite_Damage.prototype.setup = function(target) {
-    const result = target.result();
+    const parameters = PluginManager.parameters('MISS_effect');
+    const triggerWeapons = JSON.parse(parameters['TriggerWeapons']).map(Number);
+    const triggerWeapons2 = JSON.parse(parameters['TriggerWeapons2']).map(Number);
 
-    if ($gameActors.actor(1).weapons().some(weapon => [1, 2, 3, 4, 5].contains(weapon.id)) && result.missed || $gameActors.actor(1).weapons().some(weapon => [1, 2, 3, 4, 5].contains(weapon.id)) && result.evaded) {
-    if (BattleManager._subject._actorId === 1) {
-     $gameVariables.setValue(111,Math.floor(Math.random() * 4))
-     if ($gameVariables.value(111) == 0) {
-       $gameTemp.requestAnimation([target], 44);
-     }
-     if ($gameVariables.value(111) == 1) {
-       $gameTemp.requestAnimation([target], 45);
-     }
-     if ($gameVariables.value(111) == 2) {
-       $gameTemp.requestAnimation([target], 46);
-     }
-     if ($gameVariables.value(111) == 3) {
-       $gameTemp.requestAnimation([target], 47);
-     }
-
+    function isValidTargetIndex() {
+        const subject = BattleManager._subject;
+        const index = subject?._lastTargetIndex;
+        const members = $gameTroop.members();
+        return subject && index >= 0 && index < members.length && members[index].isEnemy() && members[index].enemyId() !== 20;
     }
-        this._colorType = 0;
-        this.createMiss();
-    } else if (result.hpAffected) {
-        this._colorType = result.hpDamage >= 0 ? 0 : 1;
-        this.createDigits(result.hpDamage);
-    } else if (target.isAlive() && result.mpDamage !== 0) {
-        this._colorType = result.mpDamage >= 0 ? 2 : 3;
-        this.createDigits(result.mpDamage);
-    }
-    if (result.critical) {
-        this.setupCriticalEffect();
-    }
-};
 
+    Sprite_Damage.prototype.setup = function(target) {
+        const result = target.result();
+        if (isValidTargetIndex()) {
+            const actorWeapons = $gameActors.actor(1).weapons();
+            const missedOrEvaded = result.missed || result.evaded;
+            const triggerAnimation = (weaponIds, animations) => {
+                if (actorWeapons.some(weapon => weaponIds.includes(weapon.id)) && missedOrEvaded) {
+                    if (BattleManager._subject._actorId === 1) {
+                        const animationId = animations[Math.floor(Math.random() * animations.length)];
+                        $gameTemp.requestAnimation([target], animationId);
+                    }
+                }
+            };
 
-})(); 
+            // 1番目のトリガー条件
+            triggerAnimation(triggerWeapons, [44, 45, 46, 47]);
+
+            // 2番目のトリガー条件
+            triggerAnimation(triggerWeapons2, [67, 68, 67, 68]);
+
+            if (result.hpAffected) {
+                this._colorType = result.hpDamage >= 0 ? 0 : 1;
+                this.createDigits(result.hpDamage);
+            } else if (target.isAlive() && result.mpDamage !== 0) {
+                this._colorType = result.mpDamage >= 0 ? 2 : 3;
+                this.createDigits(result.mpDamage);
+            }
+            if (result.critical) {
+                this.setupCriticalEffect();
+            }
+        }
+    };
+})();
