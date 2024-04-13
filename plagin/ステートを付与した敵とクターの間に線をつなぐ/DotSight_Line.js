a6d9b9d2a28d7f618f5ee8aaa47ab71550a2f344
garyu <49363894+garyu01@users.noreply.github.com>
2024-04-13 22:53:44 +0900

    Update DotSight_Line.js
    敵が死んだとき、ステートが解除されたときに線を消すように修正

--- plagin/ステートを付与した敵とクターの間に線をつなぐ/DotSight_Line.js
+++ plagin/ステートを付与した敵とクターの間に線をつなぐ/DotSight_Line.js
@@ -4,24 +4,24 @@
  *
  * @command SetLineProperties
  * @text Set Line Properties
- * @desc Sets the properties of the line to be drawn between characters.
+ * @desc ステートを付与した敵1体とアクターの間に線を描画
  *
  * @arg color
  * @text Line Color
- * @desc The color of the line (hexadecimal, e.g., #FF0000 for red).
+ * @desc 線の色 (16進数、例: 赤の場合は #FF0000).
  * @type string
  * @default #FF0000
  *
  * @arg width
  * @text Line Width
- * @desc The width of the line.
+ * @desc 線の太さ
  * @type number
  * @min 1
  * @default 2
  * 
  * @arg alpha
  * @text Line Alpha
- * @desc The alpha (transparency) of the line (0.0 - fully transparent, 1.0 - fully opaque).
+ * @desc 透明度 (0.0 - 完全に透明、1.0 - 完全に不透明).
  * @type number
  * @decimals 2
  * @min 0
@@ -30,42 +30,42 @@
  *
  * @arg offsetXStart
  * @text Start X Offset
- * @desc The X offset for the start of the line.
+ * @desc 線の始まりのX位置
  * @type number
  * @default -40
  *
  * @arg offsetYStart
  * @text Start Y Offset
- * @desc The Y offset for the start of the line.
+ * @desc 線の始まりのY位置
  * @type number
  * @default 1
  *
  * @arg offsetXEnd
  * @text End X Offset
- * @desc The X offset for the end of the line.
+ * @desc 線の終わりX位置
  * @type number
  * @default -1
  *
  * @arg offsetYEnd
  * @text End Y Offset
- * @desc The Y offset for the end of the line.
+ * @desc 線の終わりY位置
  * @type number
  * @default -55
  *
  * @command SetStateForLine
  * @text Set State for Line
- * @desc Sets the state ID that triggers line drawing between characters.
+ * @desc 線を描画するステートID
  *
  * @arg stateId
  * @text State ID
- * @desc The ID of the state that triggers line drawing.
+ * @desc 線を描画するステートID
  * @type number
  * @default 1
  */
-var DotSight = DotSight || {}; 
+var DotSight = DotSight || {};
 DotSight.Line = DotSight.Line || {};
-(function($) {
-    $.lineProperties = {
+(function(DotSight) {
+    DotSight.lineProperties = {
         color: '#FF0000',
         width: 2,
         alpha: 1.0,
@@ -77,17 +77,17 @@ DotSight.Line = DotSight.Line || {};
     };
 
     PluginManager.registerCommand('DotSight_Line', 'SetLineProperties', args => {
-        $.lineProperties.color = args.color;
-        $.lineProperties.width = Number(args.width);
-        $.lineProperties.alpha = Number(args.alpha);
-        $.lineProperties.offsetXStart = Number(args.offsetXStart);
-        $.lineProperties.offsetYStart = Number(args.offsetYStart);
-        $.lineProperties.offsetXEnd = Number(args.offsetXEnd);
-        $.lineProperties.offsetYEnd = Number(args.offsetYEnd);
+        DotSight.lineProperties.color = args.color;
+        DotSight.lineProperties.width = Number(args.width);
+        DotSight.lineProperties.alpha = Number(args.alpha);
+        DotSight.lineProperties.offsetXStart = Number(args.offsetXStart);
+        DotSight.lineProperties.offsetYStart = Number(args.offsetYStart);
+        DotSight.lineProperties.offsetXEnd = Number(args.offsetXEnd);
+        DotSight.lineProperties.offsetYEnd = Number(args.offsetYEnd);
     });
 
     PluginManager.registerCommand('DotSight_Line', 'SetStateForLine', args => {
-        $.lineProperties.stateId = Number(args.stateId);
+        DotSight.lineProperties.stateId = Number(args.stateId);
     });
 
     const _Spriteset_Battle_createLowerLayer = Spriteset_Battle.prototype.createLowerLayer;
@@ -103,18 +103,18 @@ DotSight.Line = DotSight.Line || {};
 
     Spriteset_Battle.prototype.createLineJoint = function(sourceSprite, targetSprite, data) {
         this._lineContainer.removeChildren();
-        const sprite = new $.Sprite_LineJoint(sourceSprite, targetSprite, data);
+        const sprite = new DotSight.Sprite_LineJoint(sourceSprite, targetSprite, data);
         this._lineContainer.addChild(sprite);
     };
 
-    $.Sprite_LineJoint = function() {
+    DotSight.Sprite_LineJoint = function() {
         this.initialize.apply(this, arguments);
     };
 
-    $.Sprite_LineJoint.prototype = Object.create(PIXI.Graphics.prototype);
-    $.Sprite_LineJoint.prototype.constructor = $.Sprite_LineJoint;
+    DotSight.Sprite_LineJoint.prototype = Object.create(PIXI.Graphics.prototype);
+    DotSight.Sprite_LineJoint.prototype.constructor = DotSight.Sprite_LineJoint;
 
-    $.Sprite_LineJoint.prototype.initialize = function(sourceSprite, targetSprite, data) {
+    DotSight.Sprite_LineJoint.prototype.initialize = function(sourceSprite, targetSprite, data) {
         PIXI.Graphics.call(this);
         this._sourceSprite = sourceSprite;
         this._targetSprite = targetSprite;
@@ -122,13 +122,13 @@ DotSight.Line = DotSight.Line || {};
         this.z = 7;
     };
 
-    $.Sprite_LineJoint.prototype.update = function() {
+    DotSight.Sprite_LineJoint.prototype.update = function() {
         this.clear();
         this.draw();
     };
 
-    $.Sprite_LineJoint.prototype.draw = function() {
-        const props = $.lineProperties;
+    DotSight.Sprite_LineJoint.prototype.draw = function() {
+        const props = DotSight.lineProperties;
         const color = parseInt(props.color.replace('#', ''), 16);
         this.lineStyle(props.width, color, props.alpha);
         const start = { x: this._sourceSprite.x + props.offsetXStart, y: this._sourceSprite.y + props.offsetYStart };
@@ -156,8 +156,10 @@ DotSight.Line = DotSight.Line || {};
 
     const _Game_Enemy_addState = Game_Enemy.prototype.addState;
     Game_Enemy.prototype.addState = function(stateId) {
+        // Ensure that state is not added if the enemy is already dead
+        if (!this.isAlive()) return;
         _Game_Enemy_addState.call(this, stateId);
-        if (stateId === $.lineProperties.stateId && !this._stateAdded) {
+        if (stateId === DotSight.lineProperties.stateId && !this._stateAdded) {
             this._stateAdded = true;
             this.drawPoisonLine();
         }
@@ -165,10 +167,26 @@ DotSight.Line = DotSight.Line || {};
 
     const _Game_Enemy_die = Game_Enemy.prototype.die;
     Game_Enemy.prototype.die = function() {
-        _Game_Enemy_die.call(this);
-        if (SceneManager._scene && SceneManager._scene._spriteset && SceneManager._scene._spriteset._lineContainer) {
-            SceneManager._scene._spriteset._lineContainer.removeChildren();
+        this.removeLineIfNecessary();
+        _Game_Enemy_die.call(this); // Call the original die function
+    };
+
+    Game_Enemy.prototype.removeLineIfNecessary = function() {
+        if (this._stateAdded) {
+            this._stateAdded = false;
+            // Ensure that the line container is properly accessed and cleared
+            if (SceneManager._scene && SceneManager._scene._spriteset && SceneManager._scene._spriteset._lineContainer) {
+                SceneManager._scene._spriteset._lineContainer.removeChildren();
+            }
         }
+    };
+
+    const _Game_Enemy_removeState = Game_Enemy.prototype.removeState;
+    Game_Enemy.prototype.removeState = function(stateId) {
+        if (stateId === DotSight.lineProperties.stateId) {
+            this.removeLineIfNecessary();
+        }
+        _Game_Enemy_removeState.call(this, stateId);
     };
 
     Game_Enemy.prototype.drawPoisonLine = function() {
@@ -178,5 +196,5 @@ DotSight.Line = DotSight.Line || {};
             SceneManager._scene._spriteset.createLineJoint(actorSprite, enemySprite, {});
         }
     };
-
 })(DotSight.Line);
+
