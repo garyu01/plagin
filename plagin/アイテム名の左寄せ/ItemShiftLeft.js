70eb897efa94e02b60ab70a19c2ce71518d4f6a4
garyu <49363894+garyu01@users.noreply.github.com>
2024-09-24 00:10:06 +0900

    Add files via upload
    スキルにも

--- plagin/アイテム名の左寄せ/ItemShiftLeft.js
+++ plagin/アイテム名の左寄せ/ItemShiftLeft.js
@@ -1,18 +1,20 @@
 /*:
- * @plugindesc 特定のメモタグがついたアイテムのテキストを左に寄せるプラグイン
+ * @plugindesc 特定のメモタグがついたアイテムおよびスキルのテキストを左に寄せるプラグイン
  *
  * @help
- * このプラグインは、<ShiftLeft>メモタグがついたアイテムのテキストを
+ * このプラグインは、<ShiftLeft: -20>のように数値を指定したメモタグがついたアイテムやスキルのテキストを
  * 左に寄せる機能を提供します。
  *
  * 使用方法:
- * アイテムのメモ欄に <ShiftLeft> と記述すると、そのアイテムのテキストが
- * 左に寄せられます。
+ * アイテムやスキルのメモ欄に <ShiftLeft: 数値> と記述すると、そのテキストが
+ * 指定された数値分だけ左に寄せられます。数値は正の値でも負の値でも設定できます。
  * このプラグインをプラグインマネージャに追加し、有効化してください。
  */
 
 (function() {
 
+    // アイテムリストでの処理
+    const _Window_ItemList_drawItem = Window_ItemList.prototype.drawItem;
     Window_ItemList.prototype.drawItem = function(index) {
         const item = this.itemAt(index);
         if (item) {
@@ -20,14 +22,25 @@
             const rect = this.itemLineRect(index);
             rect.width -= this.itemPadding();
             this.changePaintOpacity(this.isEnabled(item));
-            if (item.meta.ShiftLeft) {
-                // メモタグがある場合はテキストを左にずらす
-                this.drawItemName(item, rect.x - 20, rect.y, rect.width - numberWidth);
-            } else {
-                // メモタグがない場合は通常通りに描画
-                this.drawItemName(item, rect.x, rect.y, rect.width - numberWidth);
-            }
+            const shiftValue = item.meta.ShiftLeft ? parseInt(item.meta.ShiftLeft) || 0 : 0;
+            this.drawItemName(item, rect.x + shiftValue, rect.y, rect.width - numberWidth);
             this.drawItemNumber(item, rect.x, rect.y, rect.width);
+            this.changePaintOpacity(1);
+        }
+    };
+
+    // スキルリストでの処理
+    const _Window_SkillList_drawItem = Window_SkillList.prototype.drawItem;
+    Window_SkillList.prototype.drawItem = function(index) {
+        const skill = this.itemAt(index);
+        if (skill) {
+            const costWidth = this.costWidth();
+            const rect = this.itemLineRect(index);
+            rect.width -= this.itemPadding();
+            this.changePaintOpacity(this.isEnabled(skill));
+            const shiftValue = skill.meta.ShiftLeft ? parseInt(skill.meta.ShiftLeft) || 0 : 0;
+            this.drawItemName(skill, rect.x + shiftValue, rect.y, rect.width - costWidth);
+            this.drawSkillCost(skill, rect.x, rect.y, rect.width);
             this.changePaintOpacity(1);
         }
     };
@@ -39,4 +52,3 @@
     };
 
 })();
-
